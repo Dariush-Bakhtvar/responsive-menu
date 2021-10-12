d9864747dc62c7954812d998dd33f408bdb76a1f
Dariush <d.bakhtvar@gmail.com>
2021-10-12 08:55:43 +0330

    edit style and add new script

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const btnToggle = document.querySelector('.btn-toggle');
 const menu = document.querySelector('.Menu');
+const arrow = document.querySelector('.arrow-down');
 btnToggle.addEventListener('click', (e) => {
     e.preventDefault();
     btnToggle.classList.toggle('activeToggle');
@@ -11,5 +12,5 @@ menu.addEventListener('click', (e) => {
     let elem = document.getElementById(id);
     console.log(elem);
     elem.hidden = !elem.hidden;
-
+    arrow.classList.toggle('arrow-hover');
 });
